dd29f513aa0684da818ed779f6e145e39e8c615b
TiegoSilva <tiego.silvaa@gmail.com>
2020-02-16 18:47:46 -0300

    final code

--- App.js
+++ App.js
@@ -1,13 +1,87 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import MapView, {Marker} from 'react-native-maps'
+
+const { width } = Dimensions.get('window')
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = { 
+      region: {
+        latitude: -22.8974191,
+        longitude: -43.1802163,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421, 
+      },
+
+      interestPoints: [
+        {
+          id: 1,
+          coordinate:{
+            latitude: -22.8974191,
+            longitude: -43.1802163,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }, 
+          title:"Point 1",
+          description: "desc"
+        },
+        {
+          id: 2,
+          coordinate: {
+            latitude: -22.9121039,
+            longitude: -43.2323445,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421, 
+          },
+          title:"Point 1",
+          description: "desc"
+        },
+      ]
+    }
+  }
+
+  async goToSecondPoint(desiredPoint){
+    await this.mapView.animateToRegion(desiredPoint);
+    console.log(desiredPoint)
+  } 
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <View>
+          <MapView
+            style={styles.map}
+            initialRegion={this.state.region}
+            ref={map => this.mapView = map}
+            >
+              {
+                this.state.interestPoints.map(point => (
+                  <Marker 
+                    key={point.id}
+                    coordinate={point.coordinate}
+                    title={point.title}
+                    description={point.description} 
+                  />
+                ))
+              }
+          </MapView>
+        </View>
+
+              {/* this.state.interestPoints[1].coordinate */}
+        <View>
+          <Button onPress={() => this.goToSecondPoint(this.state.interestPoints[1].coordinate)} title="Go to second point"
+          />
+        </View>
+      </View>
+    );
+  }
+
+} 
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +90,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width,
+    height: 250
+  }
 });
+
+
+
+
+export default App;
